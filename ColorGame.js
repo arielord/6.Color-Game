d935d2bed9b7f78eb7868a968bdfccd30bae04ec
Ariel Ordonez <ariel@Ariels-MacBook-Pro.local>
2018-11-20 15:56:19 -0500

    Dyanamic button

--- ColorGame.js
+++ ColorGame.js
@@ -8,6 +8,9 @@ var resetButton = document.getElementById("reset");
 var h1 = document.querySelector("h1");
 var modeButtons = document.querySelectorAll(".mode")
 
+var streak = 3;
+var streakMultiplier = 3;
+
 init();
 
 function init(){
@@ -17,6 +20,7 @@ function init(){
 		modeButtons[0].classList.remove("selected");
 		modeButtons[1].classList.remove("selected");
 		modeButtons[2].classList.remove("selected");
+		modeButtons[3].classList.remove("selected");
 		this.classList.add("selected");
 		if(this.textContent === "Easy") {
 			numSquares = 3;
@@ -24,35 +28,63 @@ function init(){
 		else if(this.textContent === "Intermediate"){
 			numSquares = 6;
 		}
-		else {
+		else if(this.textContent === "Hard"){
 			numSquares = 9;
+		}
+		else {
+			if (streak <= 1){
+				streakMultiplier -= 1;
+			}
+			else{
+				streakMultiplier += 1;
+			}
+			numSquares = streakMultiplier*3;
+			//Add dynamic mode
+			if (numSquares > squares.length){
+				for(var i = 0; i < 3; i++){
+					var div = document.createElement("div");
+					div.classList.add("square");
+					document.getElementById("container").appendChild(div);
+				}
+				squares = document.querySelectorAll(".square");
+				clicked();
+			}
 		}
 		reset();
 		})
 	}
 
+	squares = document.querySelectorAll(".square");
+
 	//Setting up squares
+
+	clicked();
+	reset();
+}
+
+function clicked(){
 	for(var i = 0; i < squares.length; i++){
 	squares[i].style.backgroundColor = colors[i];
 	//add event listener to each square
+
 	squares[i].addEventListener("click", function(){
 		//get color of clicked square
 		var clickedColor = this.style.backgroundColor;
 		//compare color to pickedColor
 		if(clickedColor === pickedColor){
+			streak += 1;
 			messageDisplay.textContent = "Correct!";
 			h1.style.backgroundColor = pickedColor;
 			resetButton.textContent = "Play Again?"
 			changeColors(clickedColor);
 		}
 		else{
+			streak -= 1;
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again!";
 		}
 		});
 	}
-
-	reset();
 }
 
 
@@ -62,6 +94,7 @@ function reset() {
 	resetButton.textContent = "New Colors";
 	colorDisplay.textContent = pickedColor;
 	messageDisplay.textContent = "";
+	squares = document.querySelectorAll(".square");
 
 	for(var i = 0; i < squares.length; i++){
 		if(colors[i]) {
@@ -108,3 +141,12 @@ function randomColor(){
 	var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
 	return rgb;
 }
+
+// var div = document.createElement("div");
+// div.style.width = "100px";
+// div.style.height = "100px";
+// div.style.background = "red";
+// div.style.color = "white";
+// div.innerHTML = "Hello";
+
+// document.getElementById("main").appendChild(div);
